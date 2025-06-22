d2f85d25d68db3f1ca7a81ede6f1764aafc20ee0
mohamed ibrahim <112399700+muhamed-ibrahim@users.noreply.github.com>
2025-06-22 10:12:52 +0300

    feat: add admin in seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => "Mohamed Ibrahim",
+            'email' => "mohamed.ibrahim@gmail.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'two_factor_secret' => null,
@@ -30,6 +30,9 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
             'profile_photo_path' => null,
             'current_team_id' => null,
+            'phone' => '01175120011',
+            'address' => 'Cairo, Egypt',
+            'usertype' => 1
         ];
     }
 
