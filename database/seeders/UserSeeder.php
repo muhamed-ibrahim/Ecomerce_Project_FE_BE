57e6a2a3ea26dba40800fabafd0957c7a9e19a31
mohamed ibrahim <112399700+muhamed-ibrahim@users.noreply.github.com>
2025-10-04 13:54:10 +0300

    feat: new update userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,16 +14,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => 'mohamed.ibrahim@gmail.com'],
-            [
-                'name' => 'Mohamed Ibrahim',
-                'email_verified_at' => now(),
-                'password' => Hash::make('12345678'),
-                'phone' => '01175120011',
-                'address' => 'Cairo, Egypt',
-                'usertype' => 1,
-            ]
-        );
+User::firstOrCreate(
+        ['email' => 'mohamedibrahimx37@gmail.com'],
+        [
+            'name' => 'Mohamed Ibrahim',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'phone' => '01175120011',
+            'address' => 'Cairo, Egypt',
+            'usertype' => 1,
+        ]
+    );
     }
 }
